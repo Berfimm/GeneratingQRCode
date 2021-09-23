0ed5b5d5771e9fd1b789b86253e9518929a41fc2
Berfimm <58472240+Berfimm@users.noreply.github.com>
2021-09-23 15:47:37 +0300

    qrcode/url

---
+++ QRCode/QR.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace QRCode
+{
+    partial class QR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textOrUrl = new System.Windows.Forms.TextBox();
+            this.pictureQR = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(78)))), ((int)(((byte)(116)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(163)))), ((int)(((byte)(199)))));
+            this.button1.Location = new System.Drawing.Point(495, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textOrUrl
+            // 
+            this.textOrUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textOrUrl.Location = new System.Drawing.Point(130, 392);
+            this.textOrUrl.Multiline = true;
+            this.textOrUrl.Name = "textOrUrl";
+            this.textOrUrl.Size = new System.Drawing.Size(350, 38);
+            this.textOrUrl.TabIndex = 1;
+            this.textOrUrl.Text = "https://github.com/Berfimm";
+            // 
+            // pictureQR
+            // 
+            this.pictureQR.Location = new System.Drawing.Point(130, 100);
+            this.pictureQR.Name = "pictureQR";
+            this.pictureQR.Size = new System.Drawing.Size(529, 270);
+            this.pictureQR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureQR.TabIndex = 2;
+            this.pictureQR.TabStop = false;
+            // 
+            // QR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(819, 513);
+            this.Controls.Add(this.pictureQR);
+            this.Controls.Add(this.textOrUrl);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "QR";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textOrUrl;
+        private System.Windows.Forms.PictureBox pictureQR;
+    }
+}
+
---
+++ QRCode/QR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QRCode
+{
+    public partial class QR : Form
+    {
+        public QR()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRCoder.QRCodeGenerator QG = new QRCoder.QRCodeGenerator();
+            var yazi = QG.CreateQrCode(textOrUrl.Text,QRCoder.QRCodeGenerator.ECCLevel.H);
+            var qr = new QRCoder.QRCode(yazi);
+            pictureQR.Image = qr.GetGraphic(50);
+        }
+
+
+
+    }
+}
